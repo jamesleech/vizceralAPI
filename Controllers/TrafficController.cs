5becc9479eeb51d9adca1925ceed6287bbbb19f6
James Leech <leechj@me.com>
2016-10-06 21:22:14 +1000

    rename valuesController

--- Controllers/TrafficController.cs
+++ Controllers/TrafficController.cs
@@ -7,7 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 namespace VizceralAPI.Controllers
 {
     [Route("api/[controller]")]
-    public class ValuesController : Controller
+    public class TrafficController : Controller
     {
         // GET api/values
         [HttpGet]
